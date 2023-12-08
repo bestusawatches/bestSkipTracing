9aa15f13f47b2bfc3f7eb4b2d6314ee585b32f80
bestusawatches <152624377+bestusawatches@users.noreply.github.com>
2023-12-08 17:09:08 +0100

    Delete Tracing_backend/Tracing_backend directory

--- Tracing_backend/Tracing_backend/app/utils/db/connect.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
-from starlette.background import BackgroundTasks
-
-from config import DB_URI,DB_NAME
-
-mongo_client = MongoClient(DB_URI)
-db = mongo_client[DB_NAME]
-
